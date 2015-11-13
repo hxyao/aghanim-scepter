b7d6dd35ab5c5f4b67c57f8d4b94e9fc1144fd47
sduxzz <825502451@qq.com>
2015-11-13 10:56:57 +0800

    首页优化

--- radiance/src/index/view.js
+++ radiance/src/index/view.js
@@ -15,36 +15,15 @@ define(function(require) {
     view.model = null;
 
     view.render = function() {
-
         new Navbar().render('.header');
         new Slider().render('.intro', this.model.sliderContent);
-        new Footer().render('.footer');
 
         var html = etpl.render('travel-category', {
             travelCategory: this.model.travelCategory
         });
-        $('.travels').html(html);
+        $('#travels').html(html);
 
-        var allPopovers = $('.image-popover');
-        var indexOfAllPopvers = 0;
-        $.each(this.model.travelCategory, function(i, item){
-            var indexOfPlacement = 1;
-            $.each(item.travelList, function(j, travel) {
-                $(allPopovers[indexOfAllPopvers++]).popover({
-                    'container': 'body',
-                    'trigger': 'hover',
-                    'html': true,
-                    'placement': indexOfPlacement++  % 3 == 0 ? 'left' : 'right',
-                    'title': travel.title1,
-                    'content': etpl.render('travel-popover', {
-                        'travel': travel
-                    })
-                }).on('mouseenter', function(){
-                    /** 为了给这个popover添加css不影响其他的popover，这里添加一个class，通过这个class来加css*/
-                    $(".popover").addClass("travel-detail");
-                });
-            });
-        });
+         new Footer().render('.footer');
     };
 
     return view;
